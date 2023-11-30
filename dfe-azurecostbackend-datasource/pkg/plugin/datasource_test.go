d6e48dbedaa514ffb5caf87edf391411134dda6a
Martin-Belton-gov <martin.belton@education.gov.uk>
2023-11-30 15:12:49 +0000

    Added additional unit test for getCosts

--- dfe-azurecostbackend-datasource/pkg/plugin/datasource_test.go
+++ dfe-azurecostbackend-datasource/pkg/plugin/datasource_test.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"encoding/json"
+	"reflect"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 )
@@ -105,4 +107,83 @@ func TestFetchToken(t *testing.T) {
 	if token != "sample_access_token" {
 		t.Errorf("fetchToken returned an unexpected token: %s", token)
 	}
+}
+
+func TestGetCosts(t *testing.T) {
+	// Create a new instance of the Config struct
+	config := Config{
+		SubscriptionID:            "your-subscription-id",
+		AzureCostSubscriptionUrl:  "https://your-cost-management-url.com/",
+	}
+
+	costResponse := createMockCostResponse()
+	responseJSON, err := json.Marshal(costResponse)
+	if err != nil {
+		t.Errorf("Error marshalling response: %v", err)
+	}
+
+	// Create a new instance of the httptest.Server struct
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check the request method and URL
+		if r.Method != "POST" {
+			t.Errorf("Expected POST request, got %s", r.Method)
+		}
+		// if r.URL.String() != "/your-subscription-id/providers/Microsoft.CostManagement/query?api-version=2023-03-01" {
+		// 	t.Errorf("Expected URL /your-subscription-id/providers/Microsoft.CostManagement/query?api-version=2023-03-01, got %s", r.URL.String())
+		// }
+
+		// Write a mock response
+		w.WriteHeader(http.StatusOK)
+		w.Write(responseJSON)
+	}))
+
+	// Close the server when the test is done
+	defer server.Close()
+
+	// Call the getCosts function with the mock token and config
+	token := "your-mock-token"
+	config.TokenURL = server.URL
+	costs, err := getCosts(token, config)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	id := costs.ID
+	if (id == ""){
+		t.Errorf("Expected response %v, got %v", id, costs.ID)
+	}
+
+	// Check the response
+	expected := costResponse
+	if !reflect.DeepEqual(costs, expected) {
+		t.Errorf("Expected response %v, got %v", expected, costs)
+	}
+}
+
+func createMockCostResponse() CostResponse {
+	// Define the properties of the CostResponse object
+	properties := Properties{
+		NextLink: "nextLink",
+		Columns: []Column{
+			{Name: "column1", Type: "type1"},
+			{Name: "column2", Type: "type2"},
+		},
+		Rows: [][]interface{}{
+			{1.0, "row1"},
+			{2.0, "row2"},
+		},
+	}
+
+	// Create a new instance of the CostResponse struct
+	costResponse := CostResponse{
+		ID:         "id1",
+		Name:       "name1",
+		Type:       "type1",
+		Location:   "location1",
+		SKU:        "sku1",
+		ETag:       "eTag1",
+		Properties: properties,
+	}
+
+	return costResponse
 }
